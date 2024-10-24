88a730ec42002edb2668b01720511c4703fce031
FabricioSilvany <fabriciosilvany57@gmail.com>
2024-10-24 18:32:29 +0000

    Salvando

--- main.py
+++ main.py
@@ -15,14 +15,20 @@ def logo():
 #Banco de dados
 MEU_BANCO = create_engine("sqlite:///meubanco.db")
 
+#Criando conexão
+Session = sessionmaker(bind=MEU_BANCO)
+session = Session()
 
+#Tabela
+Base = declarative_base()
 
-while True:
-    logo()
-    login_matricula = input("\nInsira seu número de matricula: ").lower()
-    login_senha = int(input("Insira a senha: "))
+class Funcionario(Base):
+    __tablename__ = "funcionarios"
 
-    if login_matricula != "123" and login_senha != 123:
-        print("Matricula ou senha incorretos. \n Tente novamente")
-    else:
-        break
+    #Campos da tabela
+    id = Column("id", Integer, primary_key = True, autoincrement = True)
+    nome = Column("nome", String)
+    matricula = Column("matricula", Integer)
+    senha = Column("senha", String)
+
+    def __init__(self, nome: str, matricula: int, se)
